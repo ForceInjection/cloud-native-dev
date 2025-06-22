e48cc363223fef784ff2ad145dc1a7c3902f217f
grissom.wang <grissom.wang@transwarp.io>
2025-06-22 21:41:00 +0800

    update for delete

--- 2.1_Probes/image/entrypoint.sh
+++ 2.1_Probes/image/entrypoint.sh
@@ -2,22 +2,22 @@
 TIMESTAMP=$(date +%d-%m-%Y_%T)
 DOCUMENT_ROOT=/usr/share/nginx/html
 
-echo "${TIMESTAMP} - Container started" 
+echo "${TIMESTAMP} - 容器已启动" 
 
-# Check if START_DELAY is defined/set or not:
+# 检查是否设置了 START_DELAY 环境变量:
 if [ "${START_DELAY}" != "" ]; then
-  echo "${TIMESTAMP} - START_DELAY is set to ${START_DELAY} - Simulating a slow-starting container by sleeping for ${START_DELAY} seconds ..."
+  echo "${TIMESTAMP} - START_DELAY 设置为 ${START_DELAY} - 模拟慢启动容器，休眠 ${START_DELAY} 秒 ..."
   sleep ${START_DELAY}
 else
-  echo "${TIMESTAMP} - START_DELAY was not set, or was set to zero - not sleeping ..."
+  echo "${TIMESTAMP} - START_DELAY 未设置或设置为零 - 不进行休眠 ..."
 fi
 
-# Create index.html with a (new) time-stamp-ed message/heading:
+# 创建带有时间戳的 index.html 文件:
 TIMESTAMP=$(date +%d-%m-%Y_%T)
-MESSAGE="${TIMESTAMP} - Kubernetes probes demo - Web service started"
+MESSAGE="${TIMESTAMP} - Kubernetes 探针演示 - Web 服务已启动"
 echo "${MESSAGE}"
 echo "<h1>${MESSAGE}</h1>" > ${DOCUMENT_ROOT}/index.html
 
-# Run whatever was passed in CMD:
-echo "${TIMESTAMP} - Exec-uting: $@"
+# 执行传入的 CMD 命令:
+echo "${TIMESTAMP} - 正在执行: $@"
 exec "$@"
