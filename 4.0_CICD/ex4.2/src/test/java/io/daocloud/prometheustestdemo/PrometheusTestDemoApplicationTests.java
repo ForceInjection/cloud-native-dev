c266d73e3809fd08e4e7a6273ca8262d24077a57
grissom.wang <wang.tianqing.cn@gmail.com>
2025-06-28 06:00:22 +0800

    update for CI/CD

---
+++ 4.0_CICD/ex4.2/src/test/java/io/daocloud/prometheustestdemo/PrometheusTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.daocloud.prometheustestdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
